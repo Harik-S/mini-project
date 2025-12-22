e2347f1598fd494c0997cbf9dbf0e2d9aea862ed
harik <harik.sodhi@icloud.com>
2025-12-22 01:44:21 +0000

    added ball and platform but not physics

--- game.m
+++ game.m
@@ -29,12 +29,22 @@ x_button = 13/20 * x_max;
 v_button = x_max;
 f.Position = [200 200 w h];
 set(gcf, 'Position', get(0, 'Screensize'));
-v_0 = x_max;
-pos=[];
-vel=[];
+
 mystery=false;
 info = imfinfo(gifFilename);
 N = numel(info);
+
+mainFont='Unison Pro Bold Round';
+fallbackFont='Arial';
+
+if any(strcmpi(listfonts,mainFont))
+    fontToUse=mainFont;
+else
+    fontToUse=fallbackFont;
+end
+
+
+
 for k = 1:N
     [A,map] = imread(gifFilename, k);
     if ~isempty(map)
@@ -62,9 +72,18 @@ img=imread(file);
 num_blocks=0;
 r=0.005*x_max;
 a=x_max;
+first_time=true;
 
+platform_pos=x_max/2;
+platform_width=0.1*x_max;
+platform_height=0.01*x_max;
+platform_offset=0.05*x_max;
 
-
+v_0 = x_max;
+r_ball=0.0125*x_max;
+pos=[x_max/2, platform_offset + r_ball];
+initial_angle = (pi/2) * (rand()-0.5);
+vel=[sin(initial_angle)*v_0, cos(initial_angle)*v_0];
 while playing
     kd = getappdata(fig,'keysDown');
     if ~isempty(kd) && isKey(kd,'p') && kd('p')
@@ -74,31 +93,40 @@ while playing
     end
     if (game_state==0)
         lp = tic;
-        hold off
-        clf
         hold on
         grid off
-        axis([0 x_max 0 y_max])
-        axis equal
-        axis tight
+        fix_axes(gca,x_max,y_max);
         set(gca,'XTick',[], 'YTick', [])
-        text(x_max/2, 9*y_max/10, "so close to square????", FontSize=60, HorizontalAlignment="center", Color='w')
-        text(x_max/2, y_max*0.7, "MODE", FontSize=36, HorizontalAlignment="center", Color='w')
-        poly1=define_hex_shape(1/2 * x_max, 1/2 * x_max + 1*(1/10)*y_max, 1/10*y_max,x_max/2,0.6*y_max);
-        fill(poly1(:,1), poly1(:,2), [226/255, 227/255, 213/255], 'EdgeColor','none')
+        if (first_time)
+            clf
+            hold on
+            fix_axes(gca,x_max,y_max);
+            text_height(gca,x_max/2, 9*y_max/10, ["so close", "to square????"], 0.05*x_max*4/3, 'HorizontalAlignment', "center", 'Color','w', 'FontName',fontToUse);
+            fix_axes(gca,x_max,y_max);
+            text_height(gca,x_max/2, y_max*0.7, "MODE", 0.05*x_max, 'HorizontalAlignment', "center", 'Color','w', 'FontName',fontToUse);
+            poly1=define_hex_shape(1/2 * x_max, 1/2 * x_max + 1*(1/10)*y_max, 1/10*y_max,x_max/2,0.6*y_max);
+            fix_axes(gca,x_max,y_max);
+            fill(poly1(:,1), poly1(:,2), [226/255, 227/255, 213/255], 'EdgeColor','none')
+            poly2=define_hex_shape(1/5 * x_max, 1/5 * x_max + 1*(1/10)*y_max,1/10*y_max,x_button,0.6*y_max);
+            slider=fill(poly2(:,1), poly2(:,2), [204/255, 64/255, 99/255], 'EdgeColor', 'none');
+            fix_axes(gca,x_max,y_max);
+            text_height(gca,7/20 * x_max, 0.6*y_max, "cool", 0.05*x_max*1/2, "HorizontalAlignment","center","VerticalAlignment","middle", 'FontName',fontToUse);
+            fix_axes(gca,x_max,y_max);
+            text_height(gca,13/20 * x_max, 0.6*y_max, "chaotic", 0.05*x_max*1/2, "HorizontalAlignment","center","VerticalAlignment","middle", 'FontName',fontToUse);
+            poly3=define_hex_shape(1/2 * x_max, 1/2 * x_max + 1*(1/10)*y_max, 1/10*y_max,x_max/2,0.3*y_max);
+            fill(poly3(:,1), poly3(:,2), [112/255, 126/255, 194/255], 'EdgeColor','none')
+            fix_axes(gca,x_max,y_max);
+            text_height(gca,0.5 * x_max, 0.3*y_max, "PLAY", 0.05*x_max, "HorizontalAlignment","center","VerticalAlignment","middle", 'FontName',fontToUse);
+            poly4=define_hex_shape(1/2 * x_max, 1/2 * x_max + 1*(1/10)*y_max, 1/10*y_max,x_max/2,0.1*y_max);
+            fill(poly4(:,1), poly4(:,2), [221/255, 80/255, 191/255], 'EdgeColor','none')
+            fix_axes(gca,x_max,y_max);
+            text_height(gca,0.5 * x_max, 0.1*y_max, "WHAT????", 0.05*x_max, "HorizontalAlignment","center","VerticalAlignment","middle", 'FontName',fontToUse);
+            first_time=false;
+        end
+        fix_axes(gca,x_max,y_max);
         poly2=define_hex_shape(1/5 * x_max, 1/5 * x_max + 1*(1/10)*y_max,1/10*y_max,x_button,0.6*y_max);
-        fill(poly2(:,1), poly2(:,2), [204/255, 64/255, 99/255], 'EdgeColor', 'none')
-        text(7/20 * x_max, 0.6*y_max, "cool", "HorizontalAlignment","center","VerticalAlignment","middle", "FontSize", 24)
-        text(13/20 * x_max, 0.6*y_max, "chaotic", "HorizontalAlignment","center","VerticalAlignment","middle", "FontSize", 24)
-        poly3=define_hex_shape(1/2 * x_max, 1/2 * x_max + 1*(1/10)*y_max, 1/10*y_max,x_max/2,0.3*y_max);
-        fill(poly3(:,1), poly3(:,2), [112/255, 126/255, 194/255], 'EdgeColor','none')
-        text(0.5 * x_max, 0.3*y_max, "PLAY", "HorizontalAlignment","center","VerticalAlignment","middle", "FontSize", 36)
-        poly4=define_hex_shape(1/2 * x_max, 1/2 * x_max + 1*(1/10)*y_max, 1/10*y_max,x_max/2,0.1*y_max);
-        fill(poly4(:,1), poly4(:,2), [221/255, 80/255, 191/255], 'EdgeColor','none')
-        text(0.5 * x_max, 0.1*y_max, "WHAT????", "HorizontalAlignment","center","VerticalAlignment","middle", "FontSize", 36)
-        axis([0 x_max 0 y_max])
-        ax = gca;
-        ax.Color = '#0c0b13';
+        slider.XData=poly2(:,1);
+        slider.YData=poly2(:,2);
         ClickLoc=get(gca,'CurrentPoint');
         ClickLoc=ClickLoc(1,1:2);
         [in1,~]=inpolygon(ClickLoc(1),ClickLoc(2),poly1(:,1),poly1(:,2));
@@ -125,11 +153,10 @@ while playing
                     ones(5,1)*7,(0:4)';7,8;7,10;ones(5,1)*8,(6:10)';ones(5,1)*9,(0:4)';10,0;10,2;10,4;10,10;ones(5,1)*11,(0:4)';ones(5,1)*11,(6:10)';12,10];
             end
             [num_blocks,~]=size(blocks);
-            hold off
             clf
             hold on
             grid off
-            axis([0 x_max 0 y_max])
+            fix_axes(gca,x_max,y_max);
             set(gca,'XTick',[], 'YTick', [])
             livesImages=gobjects(num_lives,1);
             for i = 0:(num_lives-1)
@@ -146,8 +173,13 @@ while playing
                 sq = def_sq(1/105 * x_max + p*(8/105 * x_max), 0.9 * y_max - 29/35 * x_max + q*(8/105 * x_max), 1/15*x_max);
                 blockPatches(i) = patch(sq(:,1), sq(:,2), 'w', 'EdgeColor','none');
             end
-            progress = text_height(x_max-0.01*y_max,0.95*y_max,[num2str(num_blocks) '/' num2str(num_blocks)],0.05*y_max,'HorizontalAlignment', 'right', 'VerticalAlignment', 'middle', 'Color', 'w');
+            fix_axes(gca,x_max,y_max);
+            progress = text_height(gca, x_max-0.01*y_max,0.95*y_max,num2str(num_blocks),0.05*y_max,'HorizontalAlignment', 'right', 'VerticalAlignment', 'middle', 'Color', 'w', 'FontName', fontToUse);
             blockAlive = true(num_blocks,1);
+            platform = def_rt(platform_pos-(platform_width/2),platform_offset-platform_height,platform_height,platform_width);
+            platform_fill = fill(platform(:,1),platform(:,2),[1 1 1]);
+            ball = def_circ(r_ball, pos(1), pos(2), 100);
+            ball_fill = fill(ball(:,1),ball(:,2),[1 1 1]);
         end
         if (in4==1)
             game_state=1;
@@ -174,7 +206,6 @@ while playing
         end
     elseif (game_state==1)
         lp=tic;
-        hold off
         clf
         hold on
         grid off
@@ -182,17 +213,19 @@ while playing
         set(gca,'XTick',[], 'YTick', [])
         poly1=define_hex_shape(0.3 * x_max, 0.3 * x_max + 1*(1/10)*y_max, 1/10*y_max,x_max*0.3,0.9*y_max);
         fill(poly1(:,1), poly1(:,2), [202/255, 165/255, 102/255], 'EdgeColor','none')
-        text(0.3 * x_max, 0.9*y_max, "(don't come) back", "HorizontalAlignment","center","VerticalAlignment","middle", "FontSize", 36)
-        text(0.5*x_max, 0.8*y_max,["hey cute jeans", "i think you know what this is", "but if not, hiii, this is my", ...
+        fix_axes(gca,x_max,y_max);
+        text_height(gca,0.3 * x_max, 0.9*y_max, "(don't come) back",0.032*x_max, "HorizontalAlignment","center","VerticalAlignment","middle", "FontName", fontToUse)
+        fix_axes(gca,x_max,y_max);
+        text_height(gca,0.5*x_max, 0.8*y_max,["hey cute jeans", "i think you know what this is", "but if not, hiii, this is my", ...
             "winter vac matlab project.", "it's t8 mcrae themed Arkanoid", ...
             "[you're so] cool - 5 lives", "chaotic - 3 lives", "left/a - move your platform left", ...
-            "right/d - move to the right", "*the platform accelerates so be careful*", ...
+            "right/d - move to the right", "*the platform accelerates*", ...
             "feel free to send feedback to", "harik.sodhi[at]chch.ox.ac.uk", ...
-            "live now, think later,", "harik <3"], HorizontalAlignment="center", FontSize=24, VerticalAlignment="top", Color='w')
+            "live now, think later,", "harik <3"], 0.032*x_max,HorizontalAlignment="center", VerticalAlignment="top", Color='w', FontName=fontToUse)
         gif_x1=0.1*x_max;
         gif_x2=0.9*x_max;
-        gif_y2=0.05 * x_max;
-        gif_y1=0.85*x_max;
+        gif_y2=0.1 * x_max;
+        gif_y1=0.9*x_max;
         t = toc(tGifStart);
         t = mod(t, gifDuration);
         k = find(cumDelay(2:end) > t, 1);
@@ -210,6 +243,7 @@ while playing
         [in1,~]=inpolygon(ClickLoc(1),ClickLoc(2),poly1(:,1),poly1(:,2));
         if (in1 == 1)
             game_state=0;
+            first_time=true;
         end
         tep=toc(lp);
         pause(max(1/fps - tep,0))
@@ -222,13 +256,7 @@ while playing
         
         [num_blocks_left,~]=size(blocks);
         set(gca,'YDir','normal')
-        ax = gca;
-        ax.Color = '#0c0b13';
-        daspect([1 1 1])
-        ax.XLim = [0 x_max];
-        ax.YLim = [0 y_max];
-        ax.XLimMode = 'manual';
-        ax.YLimMode = 'manual';
+        fix_axes(gca,x_max,y_max);
         tep=toc(lp);
         pause(max(1/60 - tep,0))
         fps=floor(1/tep);
@@ -254,8 +282,10 @@ end
 function square = def_sq(xl,yl,s)
     square = [xl, yl; xl+s, yl; xl+s, yl+s; xl, yl+s];
 end
-function h = text_height(x, y, str, h_data, varargin)
-    ax = gca;
+function rectangle = def_rt(xl,yl,h,w)
+    rectangle = [xl, yl; xl+w, yl; xl+w, yl+h; xl, yl+h];
+end
+function h = text_height(ax, x, y, str, h_data, varargin)
     oldUnits = ax.Units;
     ax.Units = 'points';
     axPos = ax.Position;
@@ -267,6 +297,14 @@ function h = text_height(x, y, str, h_data, varargin)
     fontSize = h_data * pointsPerData;
 
     h = text(x, y, str, 'FontSize', fontSize, varargin{:});
+end
+function fix_axes(ax,x_max,y_max)
+    ax.Color = '#0c0b13';
+    daspect([1 1 1])
+    ax.XLim = [0 x_max];
+    ax.YLim = [0 y_max];
+    ax.XLimMode = 'manual';
+    ax.YLimMode = 'manual';
 end
 function keyDown(src, event)
     kd = getappdata(src, 'keysDown');
